9e81cad5c28f53419c187cc685547290050ea57e
Ricardo Cicaida Novak <ricardo.cicaida@pucpr.edu.br>
2020-02-28 21:22:57 -0300

    -Commit inicial, base do raytracing

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            rb.AddForce(Vector3.forward);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.DownArrow))
+            rb.AddForce(Vector3.back);
+    }
+}
---
+++ Assets/Scripts/RayTracingMaster.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RayTracingMaster : MonoBehaviour
+{
+    public ComputeShader RayTracingShader;
+    public Texture SkyboxTexture;
+    
+    private RenderTexture m_target;
+    private Camera m_camera;
+
+    private int m_kernel;
+
+    private void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+        m_kernel = RayTracingShader.FindKernel("CSMain");
+    }
+    
+    private void SetShaderParameters()
+    {
+        RayTracingShader.SetMatrix("_CameraToWorld", m_camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", m_camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(m_kernel,"_SkyboxTexture", SkyboxTexture);
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
+    }
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+        // Set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(m_kernel, "Result", m_target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(m_kernel, threadGroupsX, threadGroupsY, 1);
+        // Blit the result texture to the screen
+        Graphics.Blit(m_target, destination);
+    }
+    private void InitRenderTexture()
+    {
+        if (m_target == null || m_target.width != Screen.width || m_target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (m_target != null)
+                m_target.Release();
+            // Get a render target for Ray Tracing
+            m_target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            m_target.enableRandomWrite = true;
+            m_target.Create();
+        }
+    }
+}
